0a1c0e6b8fc52ee4506006176a814d967c126c71
Huck Brock <huckbrock@gmail.com>
2017-03-07 16:22:16 -0600

    arrays.js tests

--- tests/tests_array.js
+++ tests/tests_array.js
@@ -1,14 +1,18 @@
-describe('basemode array tests', function() {
-    it( 'firstPerson', function() {
+describe('basemode arrays.js tests:', function() {
+    it( 'people should be an array of all 5 names after adding Prince', function() {
+      var completeArray = [ 'Albert', 'Dev', 'Ryan', 'Antoinette', 'Prince' ];
+      assert.deepEqual( completeArray, people , ["array was created incorrectly or Prince wasn't added"]);
+    });
+    it( 'firstPerson should be set to first index in the array. CHECK', function() {
       assert.equal( 'Albert', firstPerson );
     });
-    it( 'howManyPeople', function() {
+    it( 'howManyPeople should be set to length of the array. CHECK', function() {
       assert.equal( 4, howManyPeople );
     });
-    it( 'lastPerson', function() {
-      assert.equal( 'Antoinette', lastPerson );
+    it( 'lastPerson should be people[people.length-1] to select "Antoinette". CHECK ', function() {
+      assert.equal( 'Antoinette', lastPerson);
     });
-    it( 'people', function() {
-      assert.equal( 'Prince' , people[ people.length-1 ] );
+    it( 'people should have Prince last in the array', function() {
+      assert.equal( 'Prince' , people[ people.length-1 ], ['Prince was not pushed to the array'] );
     });
 });
