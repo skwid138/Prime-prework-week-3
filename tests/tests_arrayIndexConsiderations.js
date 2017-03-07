d81ec05192290824c3ec7eb4e96a6f5fd5e0d085
Huck Brock <huckbrock@gmail.com>
2017-03-07 16:47:02 -0600

    doWhileLoops, conditionals, indexConsiderations

--- tests/tests_arrayIndexConsiderations.js
+++ tests/tests_arrayIndexConsiderations.js
@@ -1,15 +1,15 @@
-describe('basemode array index considerations', function() {
-    it('the red color', function() {
+describe('basemode arrayIndexConsiderations.js', function() {
+    it('theColorRed should select the 0 index of colors. CHECK', function() {
         assert.equal(theColorRed, 'red');
     });
-    it('the yellow color', function() {
+    it('theColorRed should select the 2nd index of colors. CHECK', function() {
         assert.equal(theColorYellow, 'yellow');
     });
     it('the forty third color', function() {
         assert.isUndefined(fortyThirdColor);
     });
 
-    it('lastItem', function() {
+    it('lastItem function should return the last item in an array that is passed to it', function() {
         var zeroThroughFour = [0, 1, 2, 3, 4];
         assert.equal(lastItem(zeroThroughFour), 4);
 
