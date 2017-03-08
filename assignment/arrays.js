727d3f488e644b4636c62d359bf4024d07c38d02
Huck Brock <huckbrock@gmail.com>
2017-03-08 15:20:10 -0600

    Update arrays.js

--- assignment/arrays.js
+++ assignment/arrays.js
@@ -1,13 +1,13 @@
 // write an array named 'people' that holds the following names: 'Albert', 'Dev', 'Ryan', 'Antoinette'
-// uncomment the following line and ______ with the appropriate code
+// uncomment the following line and replace______ with the appropriate code
 // var people = ______;
 
 // set the variable below equal to the first name in the array
-// uncomment the following line and ______ with the appropriate code
+// uncomment the following line and replace______ with the appropriate code
 // var firstPerson = ______;
 
 // set the variable below equal to the number of names in the array
-// uncomment the following line and ______ with the appropriate code
+// uncomment the following line and replace______ with the appropriate code
 // var howManyPeople = ______;
 
 // set the variable below equal to the last name in the array
