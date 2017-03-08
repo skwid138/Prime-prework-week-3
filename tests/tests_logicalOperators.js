84cf21b6cef84cf981f21246bee9af027f3dbe08
Huck Brock <huckbrock@gmail.com>
2017-03-08 14:39:59 -0600

    edits tests and writes whileLoop test

--- tests/tests_logicalOperators.js
+++ tests/tests_logicalOperators.js
@@ -1,17 +1,17 @@
-describe('basemode Logical Operators', function() {
-  it('and', function() {
+describe('basemode logicalOperators.js', function() {
+  it('&& operators array should include 1 true value', function() {
     var trueValues = andTruthTable.filter(function(boolean) {
       return boolean;
     });
     expect(numberOfTrueElements).to.be.equal(trueValues.length);
   });
-  it('or', function() {
+  it('|| operators array should include 1 false value', function() {
     var falseValues = orTruthTable.filter(function(boolean) {
       return !boolean;
     });
     expect(numberOfFalseElements).to.be.equal(falseValues.length);
   });
-  it('not', function() {
+  it('should take boolean argument and return its opposite', function() {
     expect(opposite(true)).to.be.false;
     expect(opposite(false)).to.be.true;
   });
---
+++ tests/tests_whileLoop.js
@@ -0,0 +1,5 @@
+describe('basemode whileLoop.js', function() {
+    it( 'should return the same number that is passed in with a while loop. CHECK', function() {
+      assert.equal( 17, fillCrate( 17 ) );
+    });
+});
