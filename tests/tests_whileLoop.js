f7da97c0a88fcb7aa9ca44a2ccbb9b5b9a582ba0
Huck Brock <huckbrock@gmail.com>
2017-03-09 14:48:46 -0600

    adds probably needless extra assertions

--- tests/tests_whileLoop.js
+++ tests/tests_whileLoop.js
@@ -1,5 +1,7 @@
 describe('basemode whileLoop.js', function() {
     it( 'should return the same number that is passed in with a while loop. CHECK', function() {
       assert.equal( 17, fillCrate( 17 ) );
+      assert.equal( 42, fillCrate( 42 ) );
+      assert.equal( 36, fillCrate( 36 ) );
     });
 });
