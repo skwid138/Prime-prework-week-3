6880476246ad80d51ca4f5ad5db480e7c54b86c8
Huck Brock <huckbrock@gmail.com>
2017-03-09 14:47:39 -0600

    bus test rewrite

--- tests/test_forLoop.js
+++ tests/test_forLoop.js
@@ -1,14 +1,23 @@
-describe('basemode loop tests', function() {
-    it( 'for loop test 1', function() {
-      assert.equal( 18, shoesOnTheBus( 9 ) );
-    });
-    it( 'for loop test 2', function() {
-      assert.equal( 42, shoesOnTheBus( 21 ) );
-    });
-    it( 'while loop test 1', function() {
-      assert.equal( 42, fillCrate( 42 ) );
-    });
-    it( 'while loop test 1', function() {
-      assert.equal( 36, fillCrate( 36 ) );
-    });
+describe('basemode forLoop.js', function() {
+var kids = ['Billy', 'GOAT', 'Mountain', 'Nigora', 'LL Cool J']
+
+  it( 'should log "Nice shoes!" for each index of the array', function() {
+    var spy = sinon.spy(console, 'log');
+
+    // call the function that needs to be tested
+    shoesOnTheBus(kids);
+
+    // assert that it was called the correct number of times
+    sinon.assert.callCount(spy, 5);
+    // assert that it was called with the correct value
+    sinon.assert.calledWith(spy, 'Nice shoes!');
+
+    // restore the original function
+    spy.restore();
+  });
+  it( 'should return the number of should of kids on bus', function() {
+    assert.equal( 10, shoesOnTheBus(kids) );
+    assert.equal( 0, shoesOnTheBus([]) );
+  });
 });
+
