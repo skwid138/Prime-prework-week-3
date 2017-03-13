05a487f3a20620a5ccc8079856da6702ec2cc048
Millie Walsh <millie@primeacademy.io>
2017-03-13 10:22:55 -0500

    fix solve for forLoops

--- assignment/forLoop.js
+++ assignment/forLoop.js
@@ -1,11 +1,10 @@
-
 function shoesOnTheBus( arrayOfKidsOnBus ){
   // This function accepts an array of names of kids on the bus.
   // Assume each kid on the bus is wearing 2 shoes.
   // Write a for loop to count how many shoes are on the bus AND console logs "Nice shoes!" for each student.
   // This function should return the number of shoes on the bus.
   var shoes = 0;
-  for (var i = 0; i < kidsOnTheBus.length; i++) {
+  for (var i = 0; i < arrayOfKidsOnBus.length; i++) {
     shoes+=2;
     console.log("Nice shoes!");
   } // end for
