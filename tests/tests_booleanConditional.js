8ae2cac43a347485bf8911211bfd3da70e17e675
Millie Walsh <millie@primeacademy.io>
2017-03-13 10:32:15 -0500

    test file clean up

--- tests/tests_booleanConditional.js
+++ tests/tests_booleanConditional.js
@@ -1,10 +1,10 @@
 describe('basemode booleanConditions.js', function() {
-    it( 'isCartonFull should return true if eggsInCarton == cartonCapacity', function() {
-      assert.equal( true, isCartonFull( 19, 19 ) );
-      assert.equal( true, isCartonFull( 54, 54 ) );
-    });
-    it( 'isCartonFull should return false if eggsInCarton < cartonCapacity', function() {
-      assert.equal( false, isCartonFull( 9, 19 ) );
-      assert.equal( false, isCartonFull( 39, 43 ) );
-    });
+  it( 'isCartonFull should return true if eggsInCarton == cartonCapacity', function() {
+    assert.equal( true, isCartonFull( 19, 19 ) );
+    assert.equal( true, isCartonFull( 54, 54 ) );
+  });
+  it( 'isCartonFull should return false if eggsInCarton < cartonCapacity', function() {
+    assert.equal( false, isCartonFull( 9, 19 ) );
+    assert.equal( false, isCartonFull( 39, 43 ) );
+  });
 });
