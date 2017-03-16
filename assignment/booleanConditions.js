237dcf20cbba75de8f133619ebf5209b162edd2b
Millie Walsh <millie@primeacademy.io>
2017-03-16 12:28:02 -0500

    fix typo

--- assignment/booleanConditions.js
+++ assignment/booleanConditions.js
@@ -1,4 +1,4 @@
 function isCartonFull( eggsInCarton, cartonCapacity ){
   // write an if statement checks if the carton is at cartonCapacity
   // this function should return true if so, false if not
-} // end checkIfCartonIsFull
+} // end isCartonFull
