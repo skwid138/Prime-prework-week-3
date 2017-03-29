4888cb1e0328669662dce73c3cd9bbd912394782
Millie Walsh <millie@primeacademy.io>
2017-03-29 07:12:17 -0500

    test if eggs are greater than capacity

--- tests/tests_booleanConditional.js
+++ tests/tests_booleanConditional.js
@@ -1,10 +1,14 @@
 describe('basemode booleanConditions.js', function() {
   it( 'isCartonFull should return true if eggsInCarton == cartonCapacity', function() {
+    // eggsInCarton = cartonCapacity
     assert.equal( true, isCartonFull( 19, 19 ) );
-    assert.equal( true, isCartonFull( 54, 54 ) );
+  });
+  it( 'isCartonFull should return true if eggsInCarton > cartonCapacity', function() {
+    // eggsInCarton > cartonCapacity
+    assert.equal( true, isCartonFull( 78, 19 ) );
   });
   it( 'isCartonFull should return false if eggsInCarton < cartonCapacity', function() {
+    // eggsInCarton < cartonCapacity
     assert.equal( false, isCartonFull( 9, 19 ) );
-    assert.equal( false, isCartonFull( 39, 43 ) );
   });
 });
