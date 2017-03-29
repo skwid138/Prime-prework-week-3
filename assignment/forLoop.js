bd99ced9cf88c760043c8a202720f95c5b2a4c6f
Millie Walsh <millie@primeacademy.io>
2017-03-29 08:22:41 -0500

    give them local var shoes

--- assignment/forLoop.js
+++ assignment/forLoop.js
@@ -1,11 +1,13 @@
 function shoesOnTheBus( ArrayOfKidsOnBus ){
   // This function accepts an array (ArrayOfKidsOnBus) of kids on the bus.
   // Assume each kid on the bus is wearing 2 shoes.
+  var shoes = 0; // start with zero shoes
+
   // Write a for loop to count how many shoes are on the bus
   // AND console log "Nice shoes!" for each student.
 
   // The totaling of shoes should be done inside the loop.
-  
+
 
   // This function should return the number of shoes on the bus.
 
