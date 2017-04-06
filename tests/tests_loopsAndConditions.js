06174f34009520c9de452dcb61f5466079dba2ed
Antoinette Smith <antoinette@primeacademy.io>
2017-04-06 11:24:14 -0500

    renamed files to be numbered, made 4 tests hard mode

--- tests/tests_loopsAndConditions.js
+++ tests/tests_loopsAndConditions.js
@@ -1,4 +1,4 @@
-describe('basemode loopsAndConditions.js', function() {
+describe('hardmode loopsAndConditions.js', function() {
   it('onlyEvens function should take an array argument and return only the even numbers in an array', function() {
     expect(onlyEvens([18, 22, 30])).to.be.eql([18, 22, 30]);
     expect(onlyEvens([1,2,3,4])).to.be.eql([2,4]);
