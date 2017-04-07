4aaf8182b2f94f12eb22cffa07806d02f2cf2075
Antoinette Smith <antoinette@primeacademy.io>
2017-04-07 16:33:32 -0500

    created a describe inside of a describe to see how that appears in app

--- tests/tests_arrayIndexConsiderations.js
+++ tests/tests_arrayIndexConsiderations.js
@@ -1,19 +1,21 @@
 describe('basemode arrayIndexConsiderations.js', function() {
-  it('theColorRed should select the 0 index of colors. CHECK', function() {
+  it('theColorRed selects the 0 index of colors. CHECK', function() {
       assert.equal(theColorRed, 'red');
   });
-  it('theColorYellow should select the 2nd index of colors. CHECK', function() {
+  it('theColorYellow selects the 2nd index of colors. CHECK', function() {
       assert.equal(theColorYellow, 'yellow');
   });
-  it('the forty third color', function() {
+  it('fortyThirdColor is undefined', function() {
       assert.isUndefined(fortyThirdColor);
   });
 
-  it('lastItem function should return the last item in an array that is passed to it', function() {
-      var zeroThroughFour = [4];
-      assert.equal(lastItem(zeroThroughFour), 4);
+  describe('basemode lastItem()', function() {
+    it('returns the last item in an array that is passed to it', function() {
+        var zeroThroughFour = [4];
+        assert.equal(lastItem(zeroThroughFour), 4);
 
-      var letters = ['p', 'r', 'i', 'm', 'e'];
-      assert.equal(lastItem(letters), 'e');
+        var letters = ['p', 'r', 'i', 'm', 'e'];
+        assert.equal(lastItem(letters), 'e');
+    });
   });
 });
