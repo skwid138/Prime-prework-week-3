e7d5d25c166eef51ee2c53ea8682760191947b5c
skwid138 <skwid138@gmail.com>
2017-08-01 15:17:40 -0500

    completed assignment 6

--- assignment/6_doWhileLoops.js
+++ assignment/6_doWhileLoops.js
@@ -5,7 +5,9 @@ do {
 } while (false)
 
 // uncomment the following line and replace ___ with the correct value
-// var valueOfCountAfterLoop = ___;
+var valueOfCountAfterLoop = 1;
+console.log(count);
+// the while portion of the loop is false so it only runs once for the do portion. 
 
 // You don't need to change the do while loop above, but you are welcome
 // to test your answer with console.log. Logging is your friend!
