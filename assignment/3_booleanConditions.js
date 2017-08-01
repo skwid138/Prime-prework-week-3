3374da9a0b21dfbea2797892b2e6840ffd6db5a3
skwid138 <skwid138@gmail.com>
2017-08-01 14:40:17 -0500

    completed assignment 3

--- assignment/3_booleanConditions.js
+++ assignment/3_booleanConditions.js
@@ -2,6 +2,14 @@ function isCartonFull( eggsInCarton, cartonCapacity ){
   // Write an if statement that checks if the carton is at cartonCapacity
   // or over cartonCapacity. It should return true if at or over capacity and
   // false if not.
+  if (eggsInCarton >= cartonCapacity) {
+    return true;
+  } else {
+    return false;
+  }
 } // end isCartonFull
 
 // test your function isCartonFull by calling it below and logging the result
+console.log("eggsInCarton = 12 cartonCapacity = 12 isCartonFull result: " + isCartonFull(12, 12));
+
+console.log("eggsInCarton = 6 cartonCapacity = 12 isCartonFull result: " + isCartonFull(6, 12));
