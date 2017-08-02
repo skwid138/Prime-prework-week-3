9250e4408049cba26d74e95c169e4ffa173b6ed5
skwid138 <skwid138@gmail.com>
2017-08-02 13:57:35 -0500

    added a function to test if my answer was correct added a semicolon to the loop as well

--- assignment/6_doWhileLoops.js
+++ assignment/6_doWhileLoops.js
@@ -1,13 +1,23 @@
 var count = 0;
 
 do {
-  count++
+  count++;
 } while (false)
 
 // uncomment the following line and replace ___ with the correct value
 var valueOfCountAfterLoop = 1;
 console.log(count);
-// the while portion of the loop is false so it only runs once for the do portion. 
+// the while portion of the loop is false so it only runs once for the do portion.
 
 // You don't need to change the do while loop above, but you are welcome
 // to test your answer with console.log. Logging is your friend!
+
+function compareMyAnswer (var1, var2) {
+  if (var1 === var2) {
+    return 'Correct! ' + true;
+  } else {
+    return 'Try Again! ' + false;
+  }
+}
+
+console.log(compareMyAnswer(count, valueOfCountAfterLoop));
